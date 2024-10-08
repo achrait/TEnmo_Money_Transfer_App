f5ceca46ccdf4cf8c3f92ef7c47d6866fb29361d
Hicham Achrait <hicham.achrait@gmail.com>
2024-10-08 19:19:34 -0400

    Delete tenmo-server directory

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techelevator.tenmo.dao;
-
-import com.techelevator.tenmo.model.TransferStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransferStatusRepository extends JpaRepository <TransferStatus, Integer> {
-    TransferStatus findById(int id);
-}
